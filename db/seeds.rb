ad9217d2c44d0111b89ff25c9730bbed7513f468
Yuri Reyes <reyestomas13@gmail.com>
2016-12-01 16:22:31 -0700

    Checkpoint 6 Seeding Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  User.create!(
+  email: Faker::Internet.email,
+  password: Faker::Internet.password,
+  )
+end
+
+users = User.all
+
+50.times do
+  Wiki.create!(
+  title: Faker::Lorem.sentence,
+  body: Faker::Lorem.paragraph,
+  user: users.sample,
+  private: false
+  )
+end
+
+User.create!(
+  email: 'admin@admin.com',
+  password: 'helloworld',
+  role: 'admin',
+)
+
+User.create!(
+  email: 'standard@standard.com',
+  password: 'helloworld',
+  role: 'standard',
+)
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Wiki.count} wikis created"
